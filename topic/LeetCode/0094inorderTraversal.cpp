6e70bbf7141e9899313d801fdbddd88783fbd408
Shadow <13710255965@163.com>
2020-10-25 15:03:08 +0800

    xiugai

---
+++ topic/LeetCode/0094inorderTraversal.cpp
@@ -0,0 +1,48 @@
+/* 给定一个二叉树，返回它的中序 遍历。
+
+示例:
+
+输入: [1,null,2,3]
+   1
+    \
+     2
+    /
+   3
+
+输出: [1,3,2]
+进阶: 递归算法很简单，你可以通过迭代算法完成吗？
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/binary-tree-inorder-traversal
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ 
+class Solution {
+public:
+    void inorder(TreeNode* root, vector<int>& res) 
+    {
+        if (!root)
+            return;
+        inorder(root->left, res);
+        res.push_back(root->val);
+        inorder(root->right, res);
+    }
+    vector<int> inorderTraversal(TreeNode* root) {
+        vector<int>res;
+
+        inorder(root,res);
+        return res;
+    }
+}; */
+/* 递归 */
+
