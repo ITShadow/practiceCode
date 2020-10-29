7ff5c87094a312c52b0758d58ddc117260de50c1
Shadow <13710255965@163.com>
2020-10-29 15:47:10 +0800

    xiugai

---
+++ topic/LeetCode/0144preorderTraversal.cpp
@@ -0,0 +1,66 @@
+/* 给你二叉树的根节点 root ，返回它节点值的 前序 遍历。
+
+ 
+
+示例 1：
+
+
+输入：root = [1,null,2,3]
+输出：[1,2,3]
+示例 2：
+
+输入：root = []
+输出：[]
+示例 3：
+
+输入：root = [1]
+输出：[1]
+示例 4：
+
+
+输入：root = [1,2]
+输出：[1,2]
+示例 5：
+
+
+输入：root = [1,null,2]
+输出：[1,2]
+ 
+
+提示：
+
+树中节点数目在范围 [0, 100] 内
+-100 <= Node.val <= 100
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/binary-tree-preorder-traversal
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ 
+class Solution {
+public:
+    vector<int>res;
+
+    void preorder(TreeNode *root)
+    {
+        if(root==NULL) return;
+
+        res.push_back(root->val);
+        preorder(root->left);
+        preorder(root->right);
+    }
+    vector<int> preorderTraversal(TreeNode* root) {
+        if(root==NULL)  return res;
+
+        preorder(root);
+        return res;
+    }
+}; */
