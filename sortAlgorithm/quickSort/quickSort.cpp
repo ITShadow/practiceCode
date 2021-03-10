28f0d6bde221acc88dd24505f4e61a9874caa338
Shadow <13710255965@163.com>
2021-03-10 15:58:52 +0800

    xiugai

--- sortAlgorithm/quickSort/quickSort.cpp
+++ sortAlgorithm/quickSort/quickSort.cpp
@@ -1,8 +1,9 @@
 #include<iostream>
 #include<algorithm>
+#include<vector>
 using namespace std;
 
-template<class T>
+/* template<class T>
 int __partition(T arr[],int l,int r)
 {
     T v=arr[l];
@@ -37,13 +38,43 @@ template<class T>
 void quickSort(T arr[],int n)
 {
     __quickSort(arr,0,n-1);
-}
+} */
 
-int main()
+
+
+
+void quickSort(vector<int>& arr,int left,int right)
+{
+  int low=left;
+  int high=right;
+  //下面两句的顺序一定不能混，否则会产生数组越界！！！very important！！！
+  if(low>high)//作为判断是否截止条件
+    return;
+  int k=arr[low];//额外空间k，取最左侧的一个作为衡量，最后要求左侧都比它小，右侧都比它大。
+  while(low<high)//这一轮要求把左侧小于a[low],右侧大于a[low]。
+  {
+    while(low<high&&arr[high]>=k)//右侧找到第一个小于k的停止
+    {
+      high--;
+    }
+    //这样就找到第一个比它小的了
+    arr[low]=arr[high];//放到low位置
+    while(low<high&&arr[low]<=k)//在low往右找到第一个大于k的，放到右侧a[high]位置
+    {
+      low++;
+    }
+    arr[high]=arr[low];   
+  }
+  arr[low]=k;//赋值然后左右递归分治求之
+  quickSort(arr, left, low-1);
+  quickSort(arr, low+1, right);  
+}
+ 
+ int main()
 {
 
-int arr[10]={7,1,4,8,3,9,6,2,5,10};
-    quickSort(arr,10);
+vector<int>arr={7,1,4,8,3,9,6,2,5,0};
+    quickSort(arr,0,10);
     for(int i=0;i<10;i++)
     {
         cout<<arr[i]<<" ";
